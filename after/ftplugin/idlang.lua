c4ce9bce2675dd309f6ac9a1ddf29104810f247d
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2026-01-08 13:06:03 +0700

    feat(ftplugin): add indent settings for filetypes

---
+++ after/ftplugin/idlang.lua
@@ -0,0 +1,5 @@
+-- [[ Prolog file: constraints.pro (Yarn), etc. ]]
+
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 2
+vim.bo.tabstop = 2
---
+++ after/ftplugin/rust.lua
@@ -0,0 +1,4 @@
+vim.bo.colorcolumn = '100'
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 4
+vim.bo.tabstop = 4
